ea26f160ff46b7b7dadecf6893d706d72184da3a
Claus Ibsen <claus.ibsen@gmail.com>
2017-06-15 20:33:54 +0200

    Expose hystrix stream

--- client-hystrix/src/main/java/com/foo/MyRoute.java
+++ client-hystrix/src/main/java/com/foo/MyRoute.java
@@ -12,7 +12,7 @@ public class MyRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("timer:foo?period=2000")
-            .hystrix()
+            .hystrix().id("call-helloswarm")
                 .to("http4:{{service:helloswarm}}/hello?connectionClose=true")
             .onFallback()
                 .setBody().constant("Nobody want to talk to me")
