3e9052aeab715558e0da67abc5a07b24a62e4584
Claus Ibsen <claus.ibsen@gmail.com>
2018-02-15 12:04:29 -0500

    Polished

--- hitme.sh
+++ hitme.sh
@@ -2,6 +2,7 @@
 
 while :; do
   sleep 0.500;
-  curl http://192.168.64.6:31045/hello
+  ###curl http://192.168.64.6:31045/hello
+  curl http://helloswarm-myproject.192.168.64.4.nip.io/hello
   echo "";
 done
